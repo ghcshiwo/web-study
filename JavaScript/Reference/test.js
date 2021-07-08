0366f95a50999fbb7b3a31bde55c11039478e43c
郭浩辞 <guohci@yonyou.com>
2021-07-08 20:55:19 +0800

    修改：新增值属性

---
+++ JavaScript/Reference/test.js
@@ -0,0 +1,22 @@
+/**
+ * Infinity 无穷
+ */
+
+console.log(100 * Infinity); /* Infinity */
+console.log(100 * -Infinity); /* -Infinity */
+console.log(-100 * Infinity); /* -Infinity */
+console.log(-100 * -Infinity); /* Infinity */
+console.log(Infinity * Infinity); /* Infinity */
+console.log(-Infinity * -Infinity); /* Infinity */
+console.log(Infinity * -Infinity); /* -Infinity */
+console.log(0 * Infinity); /* NaN */
+console.log(0 * -Infinity); /* NaN */
+console.log(NaN * Infinity); /* NaN */
+console.log(NaN * -Infinity); /* NaN */
+console.log(Infinity / -Infinity); /* NaN */
+console.log(Infinity / -100); /* -Infinity */
+console.log(Infinity / 100); /* Infinity */
+console.log(-Infinity / -100); /* Infinity  */
+console.log(-Infinity / 100); /* -Infinity */
+console.log(100 / Infinity); /* 0 */
+console.log(100 / -Infinity); /* -0 */
--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 重学前端相关基础知识记录
 根据MDN社区学习
 
+
 ## HTML
+## CSS 
+## JavaScript
+
+
