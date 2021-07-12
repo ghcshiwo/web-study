dec629543c5fb0fcd1be9949d922b2dbdaad93dc
郭浩辞 <guohci@yonyou.com>
2021-07-12 17:12:08 +0800

    修改：值属性完结

--- JavaScript/Reference/test.js
+++ JavaScript/Reference/test.js
@@ -20,3 +20,45 @@ console.log(-Infinity / -100); /* Infinity  */
 console.log(-Infinity / 100); /* -Infinity */
 console.log(100 / Infinity); /* 0 */
 console.log(100 / -Infinity); /* -0 */
+
+/**
+ * Infinity NaN
+ */
+
+console.log(NaN === NaN); /* false */
+console.log(Number.NaN === NaN); /* false */
+console.log(isNaN(NaN)); /* true */
+console.log(isNaN(Number.NaN)); /* true */
+console.log(isNaN('hello world')); /* true */
+console.log(Number.isNaN('hello world')); /* true */
+
+/**
+ * undefined
+ */
+// 不要这样做---start
+(function () {
+    var undefined = 'foo';
+    console.log(undefined, typeof undefined)
+})()
+// 不要这样做---end
+
+console.log(null === undefined) /* false */
+console.log(null == undefined) /* true */
+console.log(void (0) === undefined) /* true */
+
+if (typeof y === 'undefined') {       // 没有错误，执行结果为true
+    console.log("y is " + typeof y)  // y is undefined
+}
+/**
+ * globalThis
+ */
+
+var getGlobal = function () {
+    if (typeof self !== 'undefined') { return self; }
+    if (typeof window !== 'undefined') { return window; }
+    if (typeof global !== 'undefined') { return global; }
+    throw new Error('unable to locate global object');
+};
+
+var globals = getGlobal();
+console.log(globals)
